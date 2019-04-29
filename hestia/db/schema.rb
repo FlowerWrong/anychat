08fe42fbd92bda40791f7aaa13204e44f5181192
FlowerWrong <sysuyangkang@gmail.com>
2019-04-29 13:52:33 +0800

    add company model

--- hestia/db/schema.rb
+++ hestia/db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_29_054724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", comment: "公司", force: :cascade do |t|
+    t.string "name", comment: "注册名"
+    t.string "alias_name", comment: "别名"
+    t.text "intro", comment: "介绍"
+    t.string "legal_person", comment: "法人"
+    t.string "tel", comment: "电话"
+    t.string "website", comment: "官网"
+    t.string "email", comment: "邮箱"
+    t.string "address", comment: "地址"
+    t.datetime "established_at", comment: "成立日期"
+    t.string "unified_social_credit_code", comment: "统一社会信用代码"
+    t.string "logo", comment: "logo"
+    t.datetime "deleted_at", comment: "删除时间"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
