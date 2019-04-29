610d41b44dc7a0200108129e8c5752af95b70f9a
FlowerWrong <sysuyangkang@gmail.com>
2019-04-29 13:40:47 +0800

    add basic config

--- hestia/config/application.rb
+++ hestia/config/application.rb
@@ -1,19 +1,19 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -44,5 +44,12 @@ module Hestia
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.available_locales = ['zh-CN', :en]
+    config.i18n.default_locale = 'zh-CN'
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Beijing'
+    config.encoding = 'utf-8'
   end
 end
